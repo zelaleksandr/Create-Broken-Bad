a49b1fa3fcbf5ec13af847756b24a77897fcff57
jetpacker06 <90773155+jetpacker06@users.noreply.github.com>
2022-08-05 08:21:52 -0500

    Fix catalysts not cycling

---
+++ src/main/java/com/jetpacker06/CreateBrokenBad/custom/NonConsumedCatalystItem.java
@@ -0,0 +1,31 @@
+package com.jetpacker06.CreateBrokenBad.custom;
+
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.TranslatableComponent;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+public class NonConsumedCatalystItem extends Item {
+    public NonConsumedCatalystItem(Properties pProperties) {
+        super(pProperties);
+    }
+    @Override
+    public void appendHoverText(ItemStack pStack, @Nullable Level pLevel, List<Component> pTooltipComponents, TooltipFlag pIsAdvanced) {
+        pTooltipComponents.add(new TranslatableComponent("not_consumed_tooltip"));
+    }
+
+    @Override
+    public boolean hasContainerItem(ItemStack stack) {
+        return true;
+    }
+
+    @Override
+    public ItemStack getContainerItem(ItemStack itemStack) {
+        return itemStack;
+    }
+}
